85dd5aaf823196c4282e190b2f0c0bdbf20817cf
ehuey275 <hueyee@rose-hulman.edu>
2024-07-07 14:28:24 -0700

    Three Dots on Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 // src/App.js
 import React from 'react';
 import PromptFetcher from './PromptFetcher';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to My React App</h1>
+        <NavBar />
         <PromptFetcher />
       </header>
     </div>
