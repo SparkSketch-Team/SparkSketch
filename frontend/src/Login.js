95706f0673589a1b24a26dbc8eeb5345a74ee99b
ehuey275 <hueyee@rose-hulman.edu>
2024-08-04 01:11:41 -0700

    Fixed Broken Dependency, Broken hrefs

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -6,6 +6,19 @@ import Button from 'react-bootstrap/Button';
 
 function Login() {
 
+    // Handler functions for buttons
+    const handleForgotPassword = (event) => {
+        event.preventDefault();
+        // Logic to handle forgot password
+        console.log('Forgot Password clicked');
+    };
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+        // Logic to handle register
+        console.log('Register clicked');
+    };
+
     return (
         <div className='App'>
             <Navbar />
@@ -19,12 +32,24 @@ function Login() {
                 </div>
                 <div className='Remember'>
                     <label><input type='checkbox' />Remember me</label>
-                    <a href='#'>Forgot Password?</a>
+                    <button
+                        type='button'
+                        onClick={handleForgotPassword}
+                        style={{ background: 'none', border: 'none', color: 'blue', cursor: 'pointer', textDecoration: 'underline', padding: 0 }}
+                    >
+                        Forgot Password?
+                    </button>
                 </div>
                 <Button variant='warning' type='submit'>Login</Button>
                 <div className='Register'>
                     <p>Don't have an account? 
-                        <a href='#'>Register</a>
+                    <button
+                            type='button'
+                            onClick={handleRegister}
+                            style={{ background: 'none', border: 'none', color: 'blue', cursor: 'pointer', textDecoration: 'underline', padding: 0 }}
+                        >
+                            Register
+                        </button>
                     </p>
                 </div>
             </form>
