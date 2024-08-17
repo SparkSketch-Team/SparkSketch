881023c0b30ec2689a5f2b3f80c747c92df585ca
acsmaker <acsmaker21@gmail.com>
2024-08-16 21:17:32 -0700

    edit screen

---
+++ frontend/src/Edit.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './NavBar';
+import './Profile.css';
+
+
+
+function Edit() {
+    return (
+        <div className='App'> 
+            <NavBar />
+            <header className='title'>EDIT</header>
+        </div>
+    )
+}
+
+export default Edit
--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -2,21 +2,23 @@ import React from 'react';
 import NavBar from './NavBar';
 import './Profile.css';
 import Avatar from 'react-avatar';
-
+import { useNavigate } from 'react-router-dom';
 
 
 function Profile() {
+    const navigate = useNavigate();
+
     return (
         <div className='App'> 
             <NavBar />
-            <header className='App-title'>MY PROFILE</header>
+            <header className='title'>MY PROFILE</header>
             <div className='container'>
-                <Avatar name='Aids Shoe' round size='120'/>
+                <div className='avatar'><Avatar name='Aids Shoe' round size='120'/></div>
                 <p className='box'>--- <br></br>Followers<br></br>
-                <button className='button'>Edit Profile</button>
+                <button className='button' type='button' onClick={() => navigate('/edit')} >Edit Profile</button>
                 </p>
                 <p className='box'>--- <br></br>Sketches<br></br>
-                <button className='button'>Settings</button>
+                <button className='button' type='button'>Settings</button>
                 </p>
             </div>
         </div>
