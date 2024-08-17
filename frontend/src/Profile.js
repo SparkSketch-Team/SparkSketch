b335b71618bdb615d9b4b171f9831ad22bd9ae53
acsmaker <acsmaker21@gmail.com>
2024-08-16 19:35:04 -0700

    profile

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from './NavBar';
-import './App.css';
+import './Profile.css';
 import Avatar from 'react-avatar';
 
 
@@ -8,10 +8,17 @@ import Avatar from 'react-avatar';
 function Profile() {
     return (
         <div className='App'> 
-        <NavBar />
-        <div>
-            <Avatar name='Aids Shoe' round/>
-        </div>
+            <NavBar />
+            <header className='App-title'>MY PROFILE</header>
+            <div className='container'>
+                <Avatar name='Aids Shoe' round size='120'/>
+                <p className='box'>--- <br></br>Followers<br></br>
+                <button className='button'>Edit Profile</button>
+                </p>
+                <p className='box'>--- <br></br>Sketches<br></br>
+                <button className='button'>Settings</button>
+                </p>
+            </div>
         </div>
     )
 }
