019e9bd39e0bf817cf4cb6f68b7f29c0fa684af5
acsmaker <acsmaker21@gmail.com>
2024-08-22 01:41:39 -0500

    profile icon

--- frontend/src/ImageFeed.js
+++ frontend/src/ImageFeed.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './ImageFeed.css';
+import Avatar from './Avatar.js';
 import { FcLike } from "react-icons/fc";
 import { FaRegComments } from "react-icons/fa";
 
@@ -31,6 +32,7 @@ const ImageFeed = () => {
                     </button>
                     <FcLike className='like' type='button'/>
                     <FaRegComments className='comment' type='button'/>
+                    <button className='profile'><Avatar className='avatar'/></button>
                 </div>
             ))}
         </div>
