35d44a123650a4463b7e895d3d5c696c0339717d
ehuey275 <hueyee@rose-hulman.edu>
2024-08-22 13:17:01 -0700

    Image Modal

--- frontend/src/ImageFeed.js
+++ frontend/src/ImageFeed.js
@@ -1,4 +1,3 @@
-// src/components/ImageFeed.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './ImageFeed.css';
@@ -6,35 +5,55 @@ import Avatar from './Avatar.js';
 import { FcLike } from "react-icons/fc";
 import { FaRegComments } from "react-icons/fa";
 
-
 const ImageFeed = () => {
-     const [images, setImages] = useState([]);
+    const [sketches, setSketches] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedImageUrl, setSelectedImageUrl] = useState('');
+
     useEffect(() => {
-        axios.get(process.env.REACT_APP_API_URL + 'api/ImageUpload/list')
+        axios.get(process.env.REACT_APP_API_URL + 'api/ImageUpload/sketches')
             .then(response => {
                 if (response.data.success) {
-                    setImages(response.data.results);
+                    setSketches(response.data.results);
                 } else {
-                    console.error("Error fetching images:", response.data.error);
+                    console.error("Error fetching sketches:", response.data.error);
                 }
             })
             .catch(error => {
-                console.error("There was an error fetching the images!", error);
+                console.error("There was an error fetching the sketches!", error);
             });
     }, []);
 
+    const handleImageClick = (url) => {
+        setSelectedImageUrl(url);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setSelectedImageUrl('');
+    };
+
     return (
         <div className="image-feed">
-            {images.map((url, index) => (
-                <div className='interaction'>
-                    <button className='buttonimg'> 
-                        <img key={index} className="image-item" src={url} alt={`images-${index}`} id='img'/>
+            {sketches.map((sketch) => (
+                <div key={sketch.postId} className='interaction'>
+                    <button className='buttonimg' onClick={() => handleImageClick(sketch.mediaUrl)}> 
+                        <img className="image-item" src={sketch.mediaUrl} alt={`sketch-${sketch.postId}`} id='img'/>
                     </button>
                     <FcLike className='like' type='button'/>
                     <FaRegComments className='comment' type='button'/>
                     <button className='profile'><Avatar className='avatar'/></button>
                 </div>
             ))}
+
+            {isModalOpen && (
+                <div className="modal" onClick={closeModal}>
+                    <span className="close">&times;</span>
+                    <img className="modal-content" src={selectedImageUrl} alt="Expanded Sketch"/>
+                    <div className="caption">Sketch</div>
+                </div>
+            )}
         </div>
     );
 };
--- frontend/src/ViewPosts.js
+++ frontend/src/ViewPosts.js
@@ -1,19 +1,18 @@
-import React, {useEffect } from 'react';
+import React from 'react';
 import NavBar from './NavBar';
-import axios from 'axios';
 import ImageFeed from './ImageFeed';
 import './App.css'
 
 function ViewPosts() {
-    useEffect(() => {
-        axios.get(process.env.REACT_APP_API_URL + 'api/ImageUpload/list')
-            .then(response => {
-                  //  setImages(response.data);
-             })
-            .catch(error => {
-                console.error("There was an error fetching the images!", error);
-            });
-         }, []);
+    // useEffect(() => {
+    //     axios.get(process.env.REACT_APP_API_URL + 'api/ImageUpload/list')
+    //         .then(response => {
+    //               //  setImages(response.data);
+    //          })
+    //         .catch(error => {
+    //             console.error("There was an error fetching the images!", error);
+    //         });
+    //      }, []);
     return(
         <div className='App'>
             <NavBar />
