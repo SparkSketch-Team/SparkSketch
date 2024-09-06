252d689368f6372e4d7a0baa795a1118ff83786c
acsmaker <acsmaker21@gmail.com>
2024-09-06 17:31:03 -0500

    ui edits

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Avatar from './Avatar.js';
-import './App.css';
+import './Navbar.css';
 
 const logout = () => {
   localStorage.removeItem('token');
@@ -14,20 +14,19 @@ const logout = () => {
 function NavBar() {
   return (
     <div>
-      <Navbar expand="lg" className="bg-light">
+      <Navbar expand="lg" dark bgColor='dark' className='navbar navbar-custom'>
         <Container>
-            <Navbar.Brand>Sparksketch [beta]</Navbar.Brand>
+            <Navbar.Brand className='navbar navbar-brand' href="/home"><sup>[beta]</sup> SparkSketch </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="/home">Home</Nav.Link>
-                <Nav.Link href="/link">Posts</Nav.Link>
+                <Nav.Link href="/link" className='navbar navbar-text'>Explore</Nav.Link>
               </Nav>
               <Nav className='ms-auto'>
                 <Nav.Link href='/profile' className='pic'>
                   <Avatar/>
                 </Nav.Link>
-                <Nav.Link className='Logout' onClick={logout}>Log Out</Nav.Link>
+                <Nav.Link className='navbar navbar-text' onClick={logout}>Log Out</Nav.Link>
               </Nav>
             </Navbar.Collapse>
         </Container>
