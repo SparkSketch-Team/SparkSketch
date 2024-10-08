c4dfda4f00c1effd361d2b60c74339e18e00febe
acsmaker <acsmaker21@gmail.com>
2024-10-08 18:07:17 -0500

    pfp final fix

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from './NavBar';
 import './Profile.css';
 import { useNavigate } from 'react-router-dom';
 import Avatar from './Avatar';
+import axios from 'axios';
 
 
 function Profile() {
     const navigate = useNavigate();
+    const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const fetchUserId = async () => {
+      try {
+        const token = localStorage.getItem('token'); // Assuming you're storing the token in localStorage
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}api/User/GetSelfUserId`, {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          }
+        });
+        // Assuming the response has the structure { "results": "fa81cf90-2862-461b-ad8a-2c6999dbf230", "success": true, "error": null }
+        if (response.data && response.data.success) {
+          setUserId(response.data.results);
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+  
+    fetchUserId();
+  }, []);
 
     return (
         <div className='App'> 
@@ -14,7 +37,7 @@ function Profile() {
             <header className='title'>MY PROFILE</header>
             <hr/>
             <div className='container'>
-                <div className='avatar'><Avatar/></div>
+                <div className='avatar'>{userId && <Avatar userId={userId} />}</div>
                 <p className='box'>--- <br></br>Followers<br></br>
                 <button className='button' type='button' onClick={() => navigate('/edit')} >Edit Profile</button>
                 </p>
